8e9268e45a9b471f7d3ce34498cfa240d9d03215
Deepu m <mdeepu2001@gmail.com>
2023-05-29 20:27:03 +0530

    new updates navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,9 @@
 function NavBar()
 {
     return (
-        <div className="navber"></div>
+        <div className="navber">
+            <h1>navber google</h1>
+        </div>
     );
 }
 export default NavBar
